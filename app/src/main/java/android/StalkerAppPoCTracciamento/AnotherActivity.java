a2c8530034880e9d79f301d5f20ff814d97ffc9c
babastreio <57809306+babastreio@users.noreply.github.com>
2020-03-02 13:27:47 +0100

    aggiustato Another Activity

--- app/src/main/java/android/StalkerAppPoCTracciamento/AnotherActivity.java
+++ app/src/main/java/android/StalkerAppPoCTracciamento/AnotherActivity.java
@@ -2,246 +2,127 @@ package android.StalkerAppPoCTracciamento;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.JsonReader;
 import android.view.KeyEvent;
 import android.view.View;
-import android.widget.ImageView;
+
 import android.widget.TextView;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
 import com.google.firebase.auth.FirebaseAuth;
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.View;
+
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+
 import androidx.core.app.ActivityCompat;
-import android.os.Bundle;
+
 import android.Manifest;
-import android.content.Intent;
+
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
-import android.os.Bundle;
+
 import android.provider.Settings;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.os.NetworkOnMainThreadException;
+
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.maps.android.PolyUtil;
-import android.util.*;
-import java.io.IOException;
+
+
 import java.util.ArrayList;
-import android.os.AsyncTask;
-import java.lang.Thread;
-import androidx.appcompat.app.ActionBar;
-import androidx.appcompat.app.AppCompatActivity;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import com.google.firebase.auth.FirebaseAuth;
 
-/*
+
 public class AnotherActivity extends AppCompatActivity {
-    ImageView imageView;
     TextView Organizzazione;
     FirebaseAuth fAuth;
     int position;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        fAuth = FirebaseAuth.getInstance();
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_another_act);
-        //if(fAuth.getCurrentUser() != null){
-            //startActivity(new Intent(getApplicationContext(),ListaOrganizzazioni.class));
-           // finish();
-        //}
-
-        ActionBar actionBar = getSupportActionBar();
-
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setDisplayShowHomeEnabled(true);
-        }
-        Organizzazione = findViewById(R.id.titleText);
-        if (position == 0) {
-            Intent intent = getIntent();
-            Bundle bundle = this.getIntent().getExtras();
-            //int pic = bundle.getInt("image");
-            String aTitle = intent.getStringExtra("title");
-            //imageView.setImageResource(pic);
-            Organizzazione.setText(aTitle);
-            actionBar.setTitle(aTitle);
-        }
-        if (position == 1) {
-            Intent intent = getIntent();
-            Bundle bundle = this.getIntent().getExtras();
-            //int pic = bundle.getInt("image");
-            String aTitle = intent.getStringExtra("title");
-            //imageView.setImageResource(pic);
-            Organizzazione.setText(aTitle);
-            actionBar.setTitle(aTitle);
-        }
-    }
-
-    public void logout(View view) {
-        FirebaseAuth.getInstance().signOut();//logout
-        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-        finish();
-    }
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-            finish();
-            return false;
-        }
-        return super.onKeyDown(keyCode, event);
-    }
-}
-*/
-
-
-//////////////////////////////////////VERSIONE CON LE MODIFICHE DI RICCARDO/////////////////////////////////////
-public class AnotherActivity extends AppCompatActivity {
+    private String risposta;
+    private TextView t;
     private LocationManager locationManager;
     private LocationListener listener;
+    private Button b;
     private RequestQueue mQueue;
-    TextView Organizzazione;
-    TextView pos;
-    Button scarica;
-    Button aggiorna;
-    FirebaseAuth fAuth;
-    int position;
-    final  ArrayList<LatLng> poligono = new ArrayList<>();
-    final  LatLngBounds.Builder builder = new LatLngBounds.Builder();
+    final ArrayList<LatLng> poligono = new ArrayList<>();
+    final LatLngBounds.Builder builder = new LatLngBounds.Builder();
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        fAuth = FirebaseAuth.getInstance();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_another_act);
-        scarica=(Button)findViewById(R.id.coordinate);
-        aggiorna=(Button)findViewById(R.id.aggiorna);
-        //if(fAuth.getCurrentUser() != null){
-        //startActivity(new Intent(getApplicationContext(),ListaOrganizzazioni.class));
-        // finish();
-        //}
-        pos=(TextView)findViewById(R.id.text_view_result);
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true);
-            actionBar.setDisplayShowHomeEnabled(true);
-        }
-        Organizzazione = findViewById(R.id.titleText);
-        if (position == 0) {
-            Intent intent = getIntent();
-            Bundle bundle = this.getIntent().getExtras();
-            //int pic = bundle.getInt("image");
-            String aTitle = intent.getStringExtra("title");
-            //imageView.setImageResource(pic);
-            Organizzazione.setText(aTitle);
-            actionBar.setTitle(aTitle);
-        }
-        if (position == 1) {
-            Intent intent = getIntent();
-            Bundle bundle = this.getIntent().getExtras();
-            //int pic = bundle.getInt("image");
-            String aTitle = intent.getStringExtra("title");
-            //imageView.setImageResource(pic);
-            Organizzazione.setText(aTitle);
-            actionBar.setTitle(aTitle);
-        }
-        mQueue = Volley.newRequestQueue(this);
-        scarica.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Parse();
-                CostruisciPol();
-                Posizione();
+        fAuth = FirebaseAuth.getInstance();
+        t = (TextView) findViewById(R.id.text_view_result);
+        b = (Button) findViewById(R.id.coordinate);
+        Parse();
+
+        /*try {
+
+
+            JSONObject jObject = new JSONObject(risposta);
+            JSONArray jsonArray = jObject.getJSONArray("Organizzazioni");
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject org = jsonArray.getJSONObject(i);
+                String organizzazione1 = org.getString("lat");
+                String organizzazione2 = org.getString("long");
+                double o1 = Double.parseDouble(organizzazione1);
+                double o2 = Double.parseDouble(organizzazione2);
+                setCoordinate(o1, o2);
             }
-        });
-    }
-    public void logout(View view) {
-        FirebaseAuth.getInstance().signOut();//logout
-        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-        finish();
-    }
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
-            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-            finish();
-            return false;
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }*/
+        for (LatLng point : poligono) {
+            builder.include(point);
         }
-        return super.onKeyDown(keyCode, event);
-    }
-    private void Parse(){
-        String url = "https://api.myjson.com/bins/17t4ai";
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            JSONArray jsonArray = response.getJSONArray("Organizzazioni");
-                            for (int i = 0; i < jsonArray.length(); i++) {
-                                JSONObject org = jsonArray.getJSONObject(i);
-                                String organizzazione1=org.getString("lat");
-                                String organizzazione2=org.getString("long");
-                                Double o1=Double.parseDouble(organizzazione1);
-                                Double o2=Double.parseDouble(organizzazione2);
-                                setCoordinate(o1,o2);
-                                // pos.append(organizzazione1 +" "+ organizzazione2+ "\n\n");
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                error.printStackTrace();
-            }
-        });
-        mQueue.add(request);
-    }
-    private void setCoordinate(Double lat, Double lon){
-        poligono.add(new LatLng(lat,lon));
-    }
-    private void Posizione(){
+
+
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         listener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-                pos.setText(" ");
-                pos.append("\n " + location.getLongitude() + " " + location.getLatitude());
+                t.setText(" ");
+                t.append("\n " + location.getLongitude() + " " + location.getLatitude());
                 //locationManager.removeUpdates(this); BLOCCO Updates tramite pulsante
-                LatLng test = new LatLng(location.getLatitude(),location.getLongitude());
+                LatLng test = new LatLng(location.getLatitude(), location.getLongitude());
                 boolean isInsideBoundary = builder.build().contains(test); // true as the test point is inside the boundary
                 boolean isInside = PolyUtil.containsLocation(test, poligono, true); // false as the test point is outside the polygon
-                if (isInsideBoundary==true && isInside==true)
-                    pos.append("\n"+"Sei dentro");
+                if (isInsideBoundary == true && isInside == true)
+                    t.append("\n" + "Sei dentro");
                 else
-                    pos.append("\n"+"sei fuori");
+                    t.append("\n" + "sei fuori");
             }
+
             @Override
             public void onStatusChanged(String s, int i, Bundle bundle) {
             }
+
             @Override
             public void onProviderEnabled(String s) {
             }
+
             @Override
             public void onProviderDisabled(String s) {
                 Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
@@ -249,10 +130,39 @@ public class AnotherActivity extends AppCompatActivity {
             }
         };
         configure_button();
+        //if(fAuth.getCurrentUser() != null){
+        //startActivity(new Intent(getApplicationContext(),ListaOrganizzazioni.class));
+        // finish();
+        //}
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+        }
+        Organizzazione = findViewById(R.id.titleText);
+        if (position == 0) {
+            Intent intent = getIntent();
+            Bundle bundle = this.getIntent().getExtras();
+            //int pic = bundle.getInt("image");
+            String aTitle = intent.getStringExtra("title");
+            //imageView.setImageResource(pic);
+            Organizzazione.setText(aTitle);
+            actionBar.setTitle(aTitle);
+        }
+        if (position == 1) {
+            Intent intent = getIntent();
+            Bundle bundle = this.getIntent().getExtras();
+            //int pic = bundle.getInt("image");
+            String aTitle = intent.getStringExtra("title");
+            //imageView.setImageResource(pic);
+            Organizzazione.setText(aTitle);
+            actionBar.setTitle(aTitle);
+        }
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode){
+        switch (requestCode) {
             case 10:
                 configure_button();
                 break;
@@ -260,31 +170,105 @@ public class AnotherActivity extends AppCompatActivity {
                 break;
         }
     }
-    void configure_button(){
+
+    void configure_button() {
         // first check for permissions
         if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
-                        ,10);
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
+                        , 10);
             }
             return;
         }
-        aggiorna.setOnClickListener(new View.OnClickListener() {
+        // this code won't execute IF permissions are not allowed, because in the line above there is return statement.
+        b.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //noinspection MissingPermission
                 try {
+
+
+
                     locationManager.requestLocationUpdates("gps", 15000, 0, listener);
-                }
-                catch (SecurityException e) {
+                } catch (SecurityException e) {
                     e.getMessage();
                 }
             }
         });
     }
-    private void CostruisciPol(){
-        for (LatLng point : poligono) {
-            builder.include(point);
-        }
+
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();//logout
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        finish();
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+            return false;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public void Parse() {
+        Cache cache = new DiskBasedCache(getCacheDir(), 1024 * 1024); // 1MB cap
+
+        Network network = new BasicNetwork(new HurlStack());
+
+
+        mQueue = new RequestQueue(cache, network);
+
+// Start the queue
+        mQueue.start();
+        String url = "https://api.myjson.com/bins/17t4ai";
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        risposta=response;
+
+                        try {
+
+
+                            JSONObject jObject = new JSONObject(risposta);
+                            JSONArray jsonArray = jObject.getJSONArray("Organizzazioni");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject org = jsonArray.getJSONObject(i);
+                                String organizzazione1 = org.getString("lat");
+                                String organizzazione2 = org.getString("long");
+                                double o1 = Double.parseDouble(organizzazione1);
+                                double o2 = Double.parseDouble(organizzazione2);
+                                setCoordinate(o1, o2);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        for (LatLng point : poligono) {
+                            builder.include(point);
+                        }
+
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle error
+                    }
+                });
+        mQueue.add(stringRequest);
+
+
+    }
+
+    public void setCoordinate(double lat,double lon){
+        poligono.add(new LatLng(lat, lon));
+        System.out.println(lat+" "+lon);
+    }
+
+
 }
--- app/src/main/java/android/StalkerAppPoCTracciamento/ListaOrganizzazioni.java
+++ app/src/main/java/android/StalkerAppPoCTracciamento/ListaOrganizzazioni.java
@@ -1,10 +1,17 @@
 package android.StalkerAppPoCTracciamento;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,25 +19,101 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.maps.android.PolyUtil;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 
 public class ListaOrganizzazioni extends AppCompatActivity {
     ListView listaOrg;
     FirebaseAuth fAuth;
+    private LocationManager locationManager;
+    private LocationListener listener;
+    final ArrayList<LatLng> poligono = new ArrayList<>();
+    final LatLngBounds.Builder builder = new LatLngBounds.Builder();
+    private RequestQueue mQueue;
 
     String Organizzazione[] = {"Torre Archimede", "Azienda"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_organizzazioni);
+        //INIZIO A SCARICARE ORGANIZZAZIONI E TRACCIAMENTO
+        mQueue = Volley.newRequestQueue(this);
+        System.out.println(3);
+       /* Parse();
+        // Find the LatLngBounds of the Polygon
+        for (LatLng point : poligono) {
+            builder.include(point);
+        }*/
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+
+                //locationManager.removeUpdates(this); BLOCCO Updates tramite pulsante
+                System.out.println(1);
+                LatLng test = new LatLng(location.getLatitude(), location.getLongitude());
+                boolean isInsideBoundary = builder.build().contains(test); // true as the test point is inside the boundary
+                boolean isInside = PolyUtil.containsLocation(test, poligono, true); // false as the test point is outside the polygon
+                if (isInsideBoundary == true && isInside == true)
+                {Context context = getApplicationContext();
+                    CharSequence text = "Sei Dentro"+ Organizzazione[0];
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();}
+
+                else
+                {Context context = getApplicationContext();
+                    CharSequence text = "Sei Fuori"+ Organizzazione[0];
+                    int duration = Toast.LENGTH_LONG;
+                    Toast toast = Toast.makeText(context, text, duration);
+                    toast.show();}
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        };
+
+        checkPermissions();
+
+
+
+        //FINE TRACCIAMENTO
+
         listaOrg =  findViewById(R.id.ListaOrg);
         MyAdapter adapter = new MyAdapter(this, Organizzazione);
+
         listaOrg.setAdapter(adapter);
         listaOrg.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -53,6 +136,74 @@ public class ListaOrganizzazioni extends AppCompatActivity {
                 }
             }
         });
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                checkPermissions();
+                break;
+            default:
+                break;
+        }
+    }
+   public  void  checkPermissions() {
+        System.out.println(2);
+        // first check for permissions
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.INTERNET}
+                        , 10);
+            }
+            return;
+        }
+
+
+                try {
+                    poligono.add(new LatLng(45.411222,11.887317));
+                    poligono.add(new LatLng(45.411555,11.887474));
+                    poligono.add(new LatLng(45.411440,11.887946));
+                    poligono.add(new LatLng(45.411109,11.887786));
+                    for (LatLng point : poligono) {
+                        builder.include(point);
+                    }
+
+
+
+                    locationManager.requestLocationUpdates("gps", 15000, 0, listener);
+                } catch (SecurityException e) {
+                    e.getMessage();
+                }
+            }
+
+
+    public void Parse() {
+        System.out.println(4);
+        String url = "https://api.myjson.com/bins/17t4ai";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                response -> {
+                    try {
+                        JSONArray jsonArray = response.getJSONArray("Organizzazioni");
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject org = jsonArray.getJSONObject(i);
+                            String organizzazione1 = org.getString("lat");
+                            String organizzazione2 = org.getString("long");
+                            double o1 = Double.parseDouble(organizzazione1);
+                            double o2 = Double.parseDouble(organizzazione2);
+                            setCoordinate(o1, o2);
+                            // pos.append(organizzazione1 +" "+ organizzazione2+ "\n\n");
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }, error -> error.printStackTrace());
+        mQueue.add(request);
+    }
+
+    public void setCoordinate(double lat,double lon){
+        poligono.add(new LatLng(lat, lon));
+        System.out.println(5);
+
     }
     class MyAdapter extends ArrayAdapter<String> {
         Context context;
